f9a7366b1139029ff23d3e6080fb61f566676369
Minh Pham <pdminh0410@gmail.com>
2025-01-11 13:25:37 -0800

    Fix bug in word-count.c

--- word-count.c
+++ word-count.c
@@ -11,7 +11,26 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    // Validate the file name length
+    if (snprintf(NULL, 0, "wc -c < '%s'", argv[1]) >= BUFSIZE) {
+        fprintf(stderr, "Error: File name is too long.\n");
+        return -1;
+    }
+
+    // Construct the command safely using snprintf
+    char cmd[BUFSIZE];
+    if (snprintf(cmd, sizeof(cmd), "wc -c < '%s'", argv[1]) >= sizeof(cmd)) {
+        fprintf(stderr, "Error: Command buffer overflow.\n");
+        return -1;
+    }
+
+    // Execute the system command
+    int ret = system(cmd);
+    if (ret == -1) {
+        fprintf(stderr, "Error: Failed to execute system command.\n");
+        return -1;
+    }
+
+    return 0;
 }
