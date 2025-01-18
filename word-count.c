85d9409dc93d5458c55a3b7e998d09d8eed23d1b
Minh Pham <pdminh0410@gmail.com>
2025-01-18 15:45:06 -0800

    Update word-count.c

--- word-count.c
+++ word-count.c
@@ -5,6 +5,23 @@
  
 #define BUFSIZE 256
 
+int read_file(const char *filename) {
+    FILE* ptr = fopen(filename, "r");
+    
+    if (ptr == NULL) {
+        printf("File cannot be opened.\n");
+        return(-1);
+    }
+    
+    char buff[BUFSIZE];
+    
+    while (fgets(buff, BUFSIZE, ptr) != NULL) {
+        printf("%s", buff);
+    }
+        
+    return 0; 
+}
+
 unsigned long fsize(char* file)
 {
     FILE * f = fopen(file, "rb");
